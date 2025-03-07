202d63897216c6e4f585eb1428a5fa3229347ba1
Woo Jun <dbsdnwns11@naver.com>
2025-03-08 01:18:50 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        int tmp = x;
+        int sum = 0;
+        while (tmp != 0) {
+            sum += tmp % 10;
+            tmp = tmp / 10;
+        }
+        if (x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
